8458431943460bbb6167c27f7f42585bd1d211ff
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-12-19 04:49:58 -0600

    Add blob: to img-src in Content-Security-Policy

--- auth-provider/next.config.mjs
+++ auth-provider/next.config.mjs
@@ -61,7 +61,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https:;",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https: blob:; font-src 'self' data:; connect-src 'self' https:;",
           },
         ],
       },
